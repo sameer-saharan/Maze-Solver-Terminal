a43392643bda7e0b41aae8f1209fda33509a503f
Sameer Saharan <albaadigoru@gmail.com>
2024-01-05 21:32:36 +0530

    added colors in generated maze

--- maze_gen.py
+++ maze_gen.py
@@ -1,5 +1,11 @@
 import random
 
+# codes for colors
+GREEN = '\033[92m'
+RED = '\033[91m'
+BLUE = '\033[94m'
+END_COLOR = '\033[0m'
+
 def generate_maze(size, wall_percentage):
     maze = [[' ' for _ in range(size)] for _ in range(size)]
     
@@ -7,7 +13,7 @@ def generate_maze(size, wall_percentage):
     num_walls = int(size * size * wall_percentage / 100)
     for _ in range(num_walls):
         x, y = random.randint(0, size-1), random.randint(0, size-1)
-        maze[x][y] = '▓'  # Wall
+        maze[x][y] = RED + '▓' + END_COLOR  # Wall
     
     # Set start and end positions
     maze[0][0] = 'S'
@@ -17,6 +23,6 @@ def generate_maze(size, wall_percentage):
     for i in range(size):
         for j in range(size):
             if maze[i][j] == ' ':
-                maze[i][j] = '◌'  # Open Space
+                maze[i][j] = BLUE + '◌' + END_COLOR  # Open Space
     
     return maze
